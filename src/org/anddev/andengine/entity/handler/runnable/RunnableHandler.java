a416736a027c547a1aa19a0ba79858165ca78740
Nicolas Gramlich (Laptop) <nicolasgramlich@gmail.com>
2010-06-20 13:07:45 +0200

    Added RunnableHandler.

---
+++ src/org/anddev/andengine/entity/handler/runnable/RunnableHandler.java
@@ -0,0 +1,60 @@
+package org.anddev.andengine.entity.handler.runnable;
+
+import java.util.ArrayList;
+
+import org.anddev.andengine.entity.IUpdateHandler;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 10:24:39 - 18.06.2010
+ */
+public class RunnableHandler implements IUpdateHandler {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+	
+	private ArrayList<Runnable> mRunnables = new ArrayList<Runnable>();
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public void onUpdate(float pSecondsElapsed) {
+		final ArrayList<Runnable> runnables = this.mRunnables;
+		final int runnableCount = runnables.size();
+		for(int i = runnableCount - 1; i >= 0; i--) {
+			runnables.get(i).run();
+		}
+		runnables.clear();
+	}
+
+	@Override
+	public void reset() {
+		this.mRunnables.clear();
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+	
+	public void addRunnable(final Runnable pRunnable) {
+		this.mRunnables.add(pRunnable);
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

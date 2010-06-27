311344240056826bbdd72cb0834d47985c738288
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-06-27 12:10:45 +0200

    FPSCounter is now FPSLogger. New FPSCounter now does what its name suggests, it counts Frames and time and so gives access to FPS.

--- src/org/anddev/andengine/entity/util/FPSLogger.java
+++ src/org/anddev/andengine/entity/util/FPSLogger.java
@@ -1,15 +1,19 @@
 package org.anddev.andengine.entity.util;
 
 import org.anddev.andengine.entity.IUpdateHandler;
+import org.anddev.andengine.util.Debug;
+import org.anddev.andengine.util.constants.TimeConstants;
 
 /**
  * @author Nicolas Gramlich
  * @since 19:52:31 - 09.03.2010
  */
-public class FPSCounter implements IUpdateHandler {
+public class FPSLogger implements IUpdateHandler, TimeConstants {
 	// ===========================================================
 	// Constants
 	// ===========================================================
+
+	private static final float AVERAGE_DURATION_DEFAULT = 5;
 
 	// ===========================================================
 	// Fields
@@ -19,17 +23,26 @@ public class FPSCounter implements IUpdateHandler {
 
 	private int mFrames;
 
+	private float mShortestFrame = Float.MAX_VALUE;
+	private float mLongestFrame = Float.MIN_VALUE;
+
+	private final float mAverageDuration;
+
 	// ===========================================================
 	// Constructors
 	// ===========================================================
 
+	public FPSLogger() {
+		this(AVERAGE_DURATION_DEFAULT);
+	}
+
+	public FPSLogger(final float pAverageDuration) {
+		this.mAverageDuration = pAverageDuration;
+	}
+
 	// ===========================================================
 	// Getter & Setter
 	// ===========================================================
-	
-	public float getFPS() {
-		return this.mFrames / this.mSecondsElapsed;
-	}
 
 	// ===========================================================
 	// Methods for/from SuperClass/Interfaces
@@ -39,6 +52,22 @@ public class FPSCounter implements IUpdateHandler {
 	public void onUpdate(final float pSecondsElapsed) {
 		this.mFrames++;
 		this.mSecondsElapsed += pSecondsElapsed;
+
+//		Debug.d("Elapsed: " + pSecondsElapsed);
+
+		this.mShortestFrame = Math.min(this.mShortestFrame, pSecondsElapsed);
+
+		this.mLongestFrame = Math.max(this.mLongestFrame, pSecondsElapsed);
+
+		if(this.mSecondsElapsed > this.mAverageDuration){
+			Debug.d(String.format("FPS: %.2f (MIN: %.0f ms | MAX: %.0f ms)", (this.mFrames / this.mSecondsElapsed), this.mShortestFrame * MILLISECONDSPERSECOND, this.mLongestFrame * MILLISECONDSPERSECOND));
+
+			this.mSecondsElapsed -= this.mAverageDuration;
+			this.mFrames = 0;
+
+			this.mLongestFrame = Float.MIN_VALUE;
+			this.mShortestFrame = Float.MAX_VALUE;
+		}
 	}
 
 	@Override
@@ -46,6 +75,9 @@ public class FPSCounter implements IUpdateHandler {
 		this.mSecondsElapsed = 0;
 
 		this.mFrames = 0;
+
+		this.mShortestFrame = Float.MAX_VALUE;
+		this.mLongestFrame = Float.MIN_VALUE;
 	}
 
 	// ===========================================================

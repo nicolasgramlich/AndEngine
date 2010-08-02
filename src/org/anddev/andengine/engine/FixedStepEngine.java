25cb58c88059c219f8f5c36eebd13c5953243bdc
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-08-02 12:21:00 +0200

    Fixed FixedStepEngine.

--- src/org/anddev/andengine/engine/FixedStepEngine.java
+++ src/org/anddev/andengine/engine/FixedStepEngine.java
@@ -3,6 +3,9 @@ package org.anddev.andengine.engine;
 import org.anddev.andengine.engine.options.FixedStepEngineOptions;
 
 /**
+ * A subclass of {@link Engine} that tries to achieve a specific amount of updates per second. 
+ * When the time since the last update is bigger long the steplength, additional updates are executed.
+ *  
  * @author Nicolas Gramlich
  * @since 10:17:47 - 02.08.2010
  */
@@ -41,7 +44,7 @@ public class FixedStepEngine extends Engine {
 
 		final float stepLength = this.mStepLength;
 		while(this.mSecondsElapsedAccumulator >= stepLength) {
-			super.onUpdate(pSecondsElapsed);
+			super.onUpdate(stepLength);
 			this.mSecondsElapsedAccumulator -= stepLength;
 		}
 	}

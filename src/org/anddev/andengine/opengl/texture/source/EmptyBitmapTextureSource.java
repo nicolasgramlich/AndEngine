438edd70594f70f68773d354c1b2f84636e50d75
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-08-08 20:22:53 +0200

    Added EmptyBitmapTextureSource. (This time 4 realz !!!)

---
+++ src/org/anddev/andengine/opengl/texture/source/EmptyBitmapTextureSource.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package org.anddev.andengine.opengl.texture.source;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 20:20:36 - 08.08.2010
+ */
+public class EmptyBitmapTextureSource implements ITextureSource {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private final int mWidth;
+	private final int mHeight;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public EmptyBitmapTextureSource(final int pWidth, final int pHeight) {
+		this.mWidth = pWidth;
+		this.mHeight = pHeight;
+	}
+
+	@Override
+	public EmptyBitmapTextureSource clone() {
+		return new EmptyBitmapTextureSource(this.mWidth, this.mHeight);
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public int getHeight() {
+		return this.mHeight;
+	}
+
+	@Override
+	public int getWidth() {
+		return this.mWidth;
+	}
+
+	@Override
+	public Bitmap onLoadBitmap() {
+		return Bitmap.createBitmap(this.mWidth, this.mHeight, Config.ARGB_8888);
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + "(" + this.mWidth + " x " + this.mHeight + ")";
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

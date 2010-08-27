1557a1e66a6eb5fec16fdfbffee4cae97520fbe1
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-08-27 14:01:25 +0200

    Added ACTION_X constants to TouchEvent for convenience.

--- src/org/anddev/andengine/input/touch/TouchEvent.java
+++ src/org/anddev/andengine/input/touch/TouchEvent.java
@@ -15,6 +15,12 @@ public class TouchEvent {
 
 	private static final Stack<TouchEvent> TOUCHEVENT_STACK = new Stack<TouchEvent>();
 	private static final Object TOUCHEVENT_RECYCLE_LOCK = new Object();
+	
+	public static final int ACTION_CANCEL = MotionEvent.ACTION_CANCEL;
+	public static final int ACTION_DOWN = MotionEvent.ACTION_DOWN;
+	public static final int ACTION_MOVE = MotionEvent.ACTION_MOVE;
+	public static final int ACTION_OUTSIDE = MotionEvent.ACTION_OUTSIDE;
+	public static final int ACTION_UP = MotionEvent.ACTION_UP;
 
 	// ===========================================================
 	// Fields

599d751717e5b0fe75e80635e2c26f4f8d0e06eb
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-09-10 14:10:29 +0200

    Added possibility to read BogoMIPS of the CPU as a ___VERY ROUGH__ performance indicator!!!

--- src/org/anddev/andengine/util/SystemUtils.java
+++ src/org/anddev/andengine/util/SystemUtils.java
@@ -1,5 +1,10 @@
 package org.anddev.andengine.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+
 import android.os.Build;
 
 /**
@@ -10,6 +15,8 @@ public class SystemUtils {
 	// ===========================================================
 	// Constants
 	// ===========================================================
+
+	private static final String BOGOMIPS_PATTERN = "BogoMIPS[\\s]*:[\\s]*(\\d+\\.\\d+)[\\s]*\n";
 
 	// ===========================================================
 	// Fields
@@ -33,6 +40,33 @@ public class SystemUtils {
 
 	public static boolean isAndroidVersionOrHigher(final int pBuildVersionCode) {
 		return Integer.parseInt(Build.VERSION.SDK) >= pBuildVersionCode;
+	}
+
+	public static float getBogoMips() throws IllegalStateException {
+		InputStream in = null;
+		try {
+			final Process process = new ProcessBuilder(new String[] { "/system/bin/cat", "/proc/cpuinfo" }).start();
+
+			in = process.getInputStream();
+			final Scanner scanner = new Scanner(in);
+
+			final boolean matchFound = scanner.findWithinHorizon(BOGOMIPS_PATTERN, 1000) != null;
+			if(matchFound) {
+				final MatchResult mr = scanner.match();
+
+				if(mr.groupCount() > 0) {
+					return Float.parseFloat(mr.group(1));
+				} else {
+					throw new IllegalStateException();
+				}
+			} else {
+				throw new IllegalStateException();
+			}
+		} catch (final IOException e) {
+			throw new IllegalStateException(e);
+		} finally {
+			StreamUtils.closeStream(in);			
+		}
 	}
 
 	// ===========================================================

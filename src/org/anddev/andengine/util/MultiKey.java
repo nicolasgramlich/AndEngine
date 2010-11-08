6a49dc851fdf1b85eba5e3c839be50219e16d92e
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-11-08 12:27:04 +0100

    Added MultiKeyHashMap (this is very useful for caching mechanisms, i.e. RectangleVertexBuffers!).

---
+++ src/org/anddev/andengine/util/MultiKey.java
@@ -0,0 +1,89 @@
+package org.anddev.andengine.util;
+
+import java.util.Arrays;
+
+public class MultiKey<K> {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+	
+	private static final long serialVersionUID = 4465448607415788805L;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private final K[] mKeys;
+	private final int mCachedHashCode;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+	
+	public MultiKey(final K... pKeys) {
+		this.mKeys = pKeys;
+
+		this.mCachedHashCode = hash(pKeys);
+	}
+	
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	public K[] getKeys() {
+		return this.mKeys;
+	}
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public boolean equals(final Object pOther) {
+		if(pOther == this) {
+			return true;
+		}
+		if(pOther instanceof MultiKey<?>) {
+			final MultiKey<?> otherMultiKey = (MultiKey<?>) pOther;
+			return Arrays.equals(this.mKeys, otherMultiKey.mKeys);
+		}
+		return false;
+	}
+
+	public static int hash(final Object ... pKeys) {
+		int hashCode = 0;
+		for(Object key : pKeys) {
+			if(key != null) {
+				hashCode ^= key.hashCode();
+			}
+		}
+		return hashCode;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return this.mCachedHashCode;
+	}
+
+	@Override
+	public String toString() {
+		return "MultiKey" + Arrays.asList(this.mKeys).toString();
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	public K getKey(final int pIndex) {
+		return this.mKeys[pIndex];
+	}
+
+	public int size() {
+		return this.mKeys.length;
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
---
+++ src/org/anddev/andengine/util/MultiKeyHashMap.java
@@ -0,0 +1,75 @@
+package org.anddev.andengine.util;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 16:54:24 - 07.11.2010
+ */
+public class MultiKeyHashMap<K, V> extends HashMap<MultiKey<K>, V> {
+	// ===========================================================
+	// Constants
+	// ==========================================================
+
+	private static final long serialVersionUID = -6262447639526561122L;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	public V get(final K ... pKeys) {
+		final int hashCode = MultiKey.hash(pKeys);
+
+		final Iterator<Entry<MultiKey<K>, V>> it = this.entrySet().iterator();
+		while(it.hasNext()) {
+			final Entry<MultiKey<K>, V> entry = it.next();
+			final MultiKey<K> entryKey = entry.getKey();
+			if (entryKey.hashCode() == hashCode && this.isEqualKey(entryKey.getKeys(), pKeys)) {
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	private boolean isEqualKey(final K[] pKeysA, final K[] pKeysB) {
+		if (pKeysA.length != pKeysB.length) {
+			return false;
+		} else {
+			for (int i = 0; i < pKeysA.length; i++) {
+				final K keyA = pKeysA[i];
+				final K keyB = pKeysB[i];
+				if(keyA == null) {
+					if(keyB != null) {
+						return false;
+					}
+				} else {
+					if(!keyA.equals(keyB)) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

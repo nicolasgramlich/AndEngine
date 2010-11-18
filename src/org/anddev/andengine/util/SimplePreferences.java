8fce9c590d4ab5de06246b0144eb831afd36fd2e
Nicolas Gramlich (Laptop) <nicolasgramlich@gmail.com>
2010-11-18 20:05:45 +0100

    Made SimplePreferences better structured.

--- src/org/anddev/andengine/util/SimplePreferences.java
+++ src/org/anddev/andengine/util/SimplePreferences.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
-
 /**
  * @author Nicolas Gramlich
  * @since 18:55:12 - 02.08.2010
@@ -29,18 +28,18 @@ public class SimplePreferences implements Constants {
 	// Constructors
 	// ===========================================================
 
-	public static SharedPreferences getInstance(final Context ctx){
-		if(INSTANCE == null) {
-			INSTANCE = ctx.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+	public static SharedPreferences getInstance(final Context ctx) {
+		if(SimplePreferences.INSTANCE == null) {
+			SimplePreferences.INSTANCE = ctx.getSharedPreferences(SimplePreferences.PREFERENCES_NAME, Context.MODE_PRIVATE);
 		}
-		return INSTANCE;
+		return SimplePreferences.INSTANCE;
 	}
 
-	public static Editor getEditorInstance(final Context ctx){
-		if(EDITORINSTANCE == null) {
-			EDITORINSTANCE = SimplePreferences.getInstance(ctx).edit();
+	public static Editor getEditorInstance(final Context ctx) {
+		if(SimplePreferences.EDITORINSTANCE == null) {
+			SimplePreferences.EDITORINSTANCE = SimplePreferences.getInstance(ctx).edit();
 		}
-		return EDITORINSTANCE;
+		return SimplePreferences.EDITORINSTANCE;
 	}
 
 	// ===========================================================
@@ -55,27 +54,22 @@ public class SimplePreferences implements Constants {
 	// Methods
 	// ===========================================================
 
-	public static boolean isFirstAccess(final Context pCtx, final String pKey){
-		return SimplePreferences.isFirstAccess(pCtx, pKey, true);
+	public static int incrementAccessCount(final Context pCtx, final String pKey) {
+		return SimplePreferences.incrementAccessCount(pCtx, pKey, 1);
 	}
 
-	public static boolean isFirstAccess(final Context pCtx, final String pKey, final boolean pIncrement){
-		return SimplePreferences.isXthAccess(pCtx, pKey, 0);
-	}
-
-	public static boolean isXthAccess(final Context pCtx, final String pKey, final int pXthAccess){
-		return isXthAccess(pCtx, pKey, pXthAccess, true);
-	}
-
-	public static boolean isXthAccess(final Context pCtx, final String pKey, final int pXthAccess, final boolean pIncrement){
+	public static int incrementAccessCount(final Context pCtx, final String pKey, final int pIncrement) {
 		final SharedPreferences prefs = SimplePreferences.getInstance(pCtx);
-		final int xthTime = prefs.getInt(pKey, 0);
+		final int accessCount = prefs.getInt(pKey, 0);
 
-		if(pIncrement) {
-			prefs.edit().putInt(pKey, xthTime + 1).commit();
-		}
+		final int newAccessCount = accessCount + pIncrement;
+		prefs.edit().putInt(pKey, newAccessCount).commit();
 
-		return xthTime == pXthAccess;
+		return newAccessCount;
+	}
+
+	public static int getAccessCount(final Context pCtx, final String pKey) {
+		return SimplePreferences.getInstance(pCtx).getInt(pKey, 0);
 	}
 
 	// ===========================================================

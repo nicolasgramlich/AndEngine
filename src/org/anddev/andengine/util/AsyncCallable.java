7d29ef09f27fad96ab89c0bd8fbf5b34c12b5a02
NicolasGramlich <devnull@localhost>
2011-02-11 11:24:12 +0100

    Added comment to AsyncCallable.

--- src/org/anddev/andengine/util/AsyncCallable.java
+++ src/org/anddev/andengine/util/AsyncCallable.java
@@ -15,7 +15,8 @@ public interface AsyncCallable<T> {
 	// ===========================================================
 
 	/**
-	 * Computes a result, or throws an exception if unable to do so.
+	 * Computes a result asynchronously, return values and exceptions are to be handled through the callbacks.
+	 * This method is expected to return almost immediately, after starting a {@link Thread} or similar.
 	 *
 	 * @return computed result
 	 * @throws Exception if unable to compute a result

03285ab7381eef023193abde3fec0844642c6ad0
NicolasGramlich <devnull@localhost>
2011-04-04 22:54:52 +0200

    Added TimeUtils.

---
+++ src/org/anddev/andengine/util/TimeUtils.java
@@ -0,0 +1,58 @@
+package org.anddev.andengine.util;
+
+import org.anddev.andengine.util.constants.TimeConstants;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 22:48:49 - 04.04.2011
+ */
+public class TimeUtils implements TimeConstants {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	public static String formatSeconds(final int pSecondsTotal) {
+		return formatSeconds(pSecondsTotal, new StringBuilder());
+	}
+	
+	public static String formatSeconds(final int pSecondsTotal, final StringBuilder pStringBuilder) {
+		final int minutes = pSecondsTotal / SECONDSPERMINUTE;
+		final int seconds = pSecondsTotal % SECONDSPERMINUTE;
+		
+		if(minutes < 10) {
+			pStringBuilder.append('0');
+		}
+		pStringBuilder.append(minutes);
+		
+		if(seconds < 10) {
+			pStringBuilder.append('0');
+		}
+		pStringBuilder.append(seconds);
+
+		return pStringBuilder.toString();
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

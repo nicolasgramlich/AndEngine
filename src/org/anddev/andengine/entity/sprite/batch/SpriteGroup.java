e007f4a4ab6fecdb520d9e03168ce8395269e5eb
NicolasGramlich <devnull@localhost>
2011-06-21 13:16:34 +0200

    Added many add/remove-methods in SpriteGroup. (Thx to aerobowl).

--- src/org/anddev/andengine/entity/sprite/batch/SpriteGroup.java
+++ src/org/anddev/andengine/entity/sprite/batch/SpriteGroup.java
@@ -1,9 +1,12 @@
 package org.anddev.andengine.entity.sprite.batch;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import org.anddev.andengine.entity.sprite.BaseSprite;
 import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.util.IMatcher;
+import org.anddev.andengine.util.ParameterCallable;
 import org.anddev.andengine.util.SmartList;
 
 /**
@@ -41,6 +44,34 @@ public class SpriteGroup extends SpriteBatch {
 
 	public void add(final BaseSprite pBaseSprite) {
 		this.mSprites.add(pBaseSprite);
+	}
+
+	public void addAll(final Collection<? extends BaseSprite> pBaseSprites) {
+		this.mSprites.addAll(pBaseSprites);
+	}
+
+	public void remove(final BaseSprite pBaseSprite) {
+		this.mSprites.remove(pBaseSprite);
+	}
+
+	public void removeAll(final Collection<? extends BaseSprite> pBaseSprites) {
+		this.mSprites.removeAll(pBaseSprites);
+	}
+
+	public void remove(final BaseSprite pBaseSprite, final ParameterCallable<BaseSprite> pBaseSpriteParameterCallable) {
+		this.mSprites.remove(pBaseSprite, pBaseSpriteParameterCallable);
+	}
+
+	public void remove(final IMatcher<BaseSprite> pBaseSpriteMatcher) {
+		this.mSprites.remove(pBaseSpriteMatcher);
+	}
+
+	public void removeAll(final IMatcher<BaseSprite> pBaseSpriteMatcher) {
+		this.mSprites.removeAll(pBaseSpriteMatcher);
+	}
+
+	public void removeAll(final IMatcher<BaseSprite> pBaseSpriteMatcher, final ParameterCallable<BaseSprite> pBaseSpriteParameterCallable) {
+		this.mSprites.removeAll(pBaseSpriteMatcher, pBaseSpriteParameterCallable);
 	}
 
 	@Override

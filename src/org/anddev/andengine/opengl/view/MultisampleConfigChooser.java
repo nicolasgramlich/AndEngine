1e2d3c998c94277838115c5e175e759051767bc3
Nicolas Gramlich <ngramlich@zynga.com>
2011-08-08 22:44:08 -0400

    Cleanup in MultisampleConfigChooser.

--- src/org/anddev/andengine/opengl/view/MultisampleConfigChooser.java
+++ src/org/anddev/andengine/opengl/view/MultisampleConfigChooser.java
@@ -4,8 +4,6 @@ import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.egl.EGLDisplay;
 
-import org.anddev.andengine.util.Debug;
-
 import android.opengl.GLSurfaceView;
 
 /**
@@ -25,14 +23,14 @@ public class MultisampleConfigChooser implements GLSurfaceView.EGLConfigChooser 
 
 	private static final int[] BUFFER = new int[1];
 
-	private static final int EGL_OPENGL_ES2_BIT = 4;
+	private static final int EGL_GLES2_BIT = 4;
 
-	private static final int[] EGLCONFIG_MULTISAMPLE = {
+	private static final int[] EGLCONFIG_ATTRIBUTES_MULTISAMPLE = {
 		EGL10.EGL_RED_SIZE, 5,
 		EGL10.EGL_GREEN_SIZE, 6,
 		EGL10.EGL_BLUE_SIZE, 5,
 		EGL10.EGL_DEPTH_SIZE, 0,
-		EGL10.EGL_RENDERABLE_TYPE, MultisampleConfigChooser.EGL_OPENGL_ES2_BIT,
+		EGL10.EGL_RENDERABLE_TYPE, MultisampleConfigChooser.EGL_GLES2_BIT,
 		EGL10.EGL_SAMPLE_BUFFERS, 1,
 		EGL10.EGL_SAMPLES, MultisampleConfigChooser.MULTISAMPLE_COUNT,
 		EGL10.EGL_NONE
@@ -41,23 +39,23 @@ public class MultisampleConfigChooser implements GLSurfaceView.EGLConfigChooser 
 	private static final int EGL_COVERAGE_BUFFERS_NV = 0x30E0;
 	private static final int EGL_COVERAGE_SAMPLES_NV = 0x30E1;
 
-	private static final int[] EGLCONFIG_COVERAGEMULTISAMPLE_NVIDIA = new int[]{
+	private static final int[] EGLCONFIG_ATTRIBUTES_COVERAGEMULTISAMPLE_NVIDIA = new int[]{
 		EGL10.EGL_RED_SIZE, 5,
 		EGL10.EGL_GREEN_SIZE, 6,
 		EGL10.EGL_BLUE_SIZE, 5,
 		EGL10.EGL_DEPTH_SIZE, 0,
-		EGL10.EGL_RENDERABLE_TYPE, MultisampleConfigChooser.EGL_OPENGL_ES2_BIT,
+		EGL10.EGL_RENDERABLE_TYPE, MultisampleConfigChooser.EGL_GLES2_BIT,
 		MultisampleConfigChooser.EGL_COVERAGE_BUFFERS_NV, 1,
 		MultisampleConfigChooser.EGL_COVERAGE_SAMPLES_NV, MultisampleConfigChooser.MULTISAMPLE_COUNT,  // always 5 in practice on tegra 2
 		EGL10.EGL_NONE
 	};
 
-	private static final int[] EGLCONFIG_FALLBACK = new int[] {
+	private static final int[] EGLCONFIG_ATTRIBUTES_FALLBACK = new int[] {
 		EGL10.EGL_RED_SIZE, 5,
 		EGL10.EGL_GREEN_SIZE, 6,
 		EGL10.EGL_BLUE_SIZE, 5,
 		EGL10.EGL_DEPTH_SIZE, 0,
-		EGL10.EGL_RENDERABLE_TYPE, MultisampleConfigChooser.EGL_OPENGL_ES2_BIT,
+		EGL10.EGL_RENDERABLE_TYPE, MultisampleConfigChooser.EGL_GLES2_BIT,
 		EGL10.EGL_NONE
 	};
 
@@ -87,58 +85,56 @@ public class MultisampleConfigChooser implements GLSurfaceView.EGLConfigChooser 
 	public EGLConfig chooseConfig(final EGL10 pEGL, final EGLDisplay pEGLDisplay) {
 		MultisampleConfigChooser.BUFFER[0] = 0;
 
-		if(pEGL.eglChooseConfig(pEGLDisplay, MultisampleConfigChooser.EGLCONFIG_MULTISAMPLE, null, 0, MultisampleConfigChooser.BUFFER) == false) {
-			throw new IllegalArgumentException("EGLCONFIG_MULTISAMPLE failed!");
-		}
-		int numConfigs = MultisampleConfigChooser.BUFFER[0];
+		final int[] eglConfigAttributes;
+		int eglConfigCount = this.getEGLConfigCount(pEGL, pEGLDisplay, MultisampleConfigChooser.EGLCONFIG_ATTRIBUTES_MULTISAMPLE);
 
-		if(numConfigs > 0) {
-			Debug.d("Using MultiSampling EGLConfig!");
+		if(eglConfigCount > 0) {
+			eglConfigAttributes = MultisampleConfigChooser.EGLCONFIG_ATTRIBUTES_MULTISAMPLE;
 		} else {
-			if(pEGL.eglChooseConfig(pEGLDisplay, MultisampleConfigChooser.EGLCONFIG_COVERAGEMULTISAMPLE_NVIDIA, null, 0, MultisampleConfigChooser.BUFFER) == false) {
-				throw new IllegalArgumentException("EGLCONFIG_COVERAGEMULTISAMPLE_NVIDIA failed!");
-			}
-			numConfigs = MultisampleConfigChooser.BUFFER[0];
+			eglConfigCount = this.getEGLConfigCount(pEGL, pEGLDisplay, MultisampleConfigChooser.EGLCONFIG_ATTRIBUTES_COVERAGEMULTISAMPLE_NVIDIA);
 
-			if(numConfigs > 0) {
+			if(eglConfigCount > 0) {
 				this.mCoverageMultiSampling = true;
-				Debug.d("Using CoverageMultiSampling EGLConfig!");
+				eglConfigAttributes = MultisampleConfigChooser.EGLCONFIG_ATTRIBUTES_COVERAGEMULTISAMPLE_NVIDIA;
 			} else {
-				if(pEGL.eglChooseConfig(pEGLDisplay, MultisampleConfigChooser.EGLCONFIG_FALLBACK, null, 0, MultisampleConfigChooser.BUFFER) == false) {
-					throw new IllegalArgumentException("EGLCONFIG_FALLBACK failed!");
-				}
-				numConfigs = MultisampleConfigChooser.BUFFER[0];
+				eglConfigCount = this.getEGLConfigCount(pEGL, pEGLDisplay, MultisampleConfigChooser.EGLCONFIG_ATTRIBUTES_FALLBACK);
 
-				if(numConfigs > 0) {
-					Debug.d("Using fallback EGLConfig!");
+				if(eglConfigCount > 0) {
+					eglConfigAttributes = MultisampleConfigChooser.EGLCONFIG_ATTRIBUTES_FALLBACK;
 				} else {
 					throw new IllegalArgumentException("No EGLConfig found!");
 				}
 			}
 		}
 
-		// Get all matching configurations.
-		final EGLConfig[] configs = new EGLConfig[numConfigs];
-		if(!pEGL.eglChooseConfig(pEGLDisplay, MultisampleConfigChooser.EGLCONFIG_MULTISAMPLE, configs, numConfigs, MultisampleConfigChooser.BUFFER)) {
+		final EGLConfig[] eglConfigs = new EGLConfig[eglConfigCount];
+		if(!pEGL.eglChooseConfig(pEGLDisplay, eglConfigAttributes, eglConfigs, eglConfigCount, MultisampleConfigChooser.BUFFER)) {
 			throw new IllegalArgumentException("eglChooseConfig failed!");
 		}
 
-		return this.findEGLConfig(pEGL, pEGLDisplay, configs);
-	}
-
-	private EGLConfig findEGLConfig(final EGL10 pEGL, final EGLDisplay pEGLDisplay, final EGLConfig[] pEGLConfigs) {
-		for(int i = 0; i < pEGLConfigs.length; ++i) {
-			final EGLConfig config = pEGLConfigs[i];
-			if(this.getConfigAttrib(pEGL, pEGLDisplay, config, EGL10.EGL_RED_SIZE, 0) == 5) {
-				return config;
-			}
-		}
-		throw new IllegalArgumentException("No EGLConfig found!");
+		return this.findEGLConfig(pEGL, pEGLDisplay, eglConfigs);
 	}
 
 	// ===========================================================
 	// Methods
 	// ===========================================================
+
+	private int getEGLConfigCount(final EGL10 pEGL, final EGLDisplay pEGLDisplay, final int[] pEGLConfigAttributes) {
+		if(pEGL.eglChooseConfig(pEGLDisplay, pEGLConfigAttributes, null, 0, MultisampleConfigChooser.BUFFER) == false) {
+			throw new IllegalArgumentException("EGLCONFIG_FALLBACK failed!");
+		}
+		return MultisampleConfigChooser.BUFFER[0];
+	}
+
+	private EGLConfig findEGLConfig(final EGL10 pEGL, final EGLDisplay pEGLDisplay, final EGLConfig[] pEGLConfigs) {
+		for(int i = 0; i < pEGLConfigs.length; ++i) {
+			final EGLConfig config = pEGLConfigs[i];
+			if(config != null && this.getConfigAttrib(pEGL, pEGLDisplay, config, EGL10.EGL_RED_SIZE, 0) == 5) {
+				return config;
+			}
+		}
+		throw new IllegalArgumentException("No EGLConfig found!");
+	}
 
 	private int getConfigAttrib(final EGL10 pEGL, final EGLDisplay pEGLDisplay, final EGLConfig pEGLConfig, final int pAttribute, final int pDefaultValue) {
 		if(pEGL.eglGetConfigAttrib(pEGLDisplay, pEGLConfig, pAttribute, MultisampleConfigChooser.BUFFER)) {

8695aab404f1923321233e3239e0a4dd4f17efdb
Nicolas Gramlich <ngramlich@zynga.com>
2011-09-07 21:14:22 +0200

    Added some more basic colors.

--- src/org/anddev/andengine/util/color/Color.java
+++ src/org/anddev/andengine/util/color/Color.java
@@ -16,15 +16,21 @@ public class Color {
 	public static final Color WHITE = new Color(1, 1, 1, 1);
 	public static final Color BLACK = new Color(0, 0, 0, 1);
 	public static final Color RED = new Color(1, 0, 0, 1);
+	public static final Color YELLOW = new Color(1, 1, 0, 1);
 	public static final Color GREEN = new Color(0, 1, 0, 1);
+	public static final Color CYAN = new Color(0, 1, 1, 1);
 	public static final Color BLUE = new Color(0, 0, 1, 1);
+	public static final Color PINK = new Color(1, 0, 1, 1);
 	public static final Color TRANSPARENT = new Color(1, 1, 1, 0);
 	
 	public static final float WHITE_PACKED = Color.WHITE.getPacked();
 	public static final float BLACK_PACKED = Color.BLACK.getPacked();
 	public static final float RED_PACKED = Color.RED.getPacked();
+	public static final float YELLOW_PACKED = Color.YELLOW.getPacked();
 	public static final float GREEN_PACKED = Color.GREEN.getPacked();
+	public static final float CYAN_PACKED = Color.CYAN.getPacked();
 	public static final float BLUE_PACKED = Color.BLUE.getPacked();
+	public static final float PINK_PACKED = Color.PINK.getPacked();
 	public static final float TRANSPARENT_PACKED = Color.TRANSPARENT.getPacked();
 
 	// ===========================================================

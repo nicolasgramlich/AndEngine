a26ab34ae5e8227d024b46364bf28fb03bae28f1
Nicolas Gramlich <ngramlich@zynga.com>
2011-10-12 18:18:32 +0200

    Added LogcicalAndShaderSourceCriteria and LogicalOrShaderSourceCriteria.

---
+++ src/org/anddev/andengine/opengl/shader/source/criteria/LogicalAndShaderSourceCriteria.java
@@ -0,0 +1,53 @@
+package org.anddev.andengine.opengl.shader.source.criteria;
+
+/**
+ * (c) Zynga 2011
+ *
+ * @author Nicolas Gramlich <ngramlich@zynga.com>
+ * @since 18:15:55 - 12.10.2011
+ */
+public class LogicalAndShaderSourceCriteria implements IShaderSourceCriteria {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+	
+	private final IShaderSourceCriteria[] mShaderSourceCriterias;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+	
+	public LogicalAndShaderSourceCriteria(final IShaderSourceCriteria ... pShaderSourceCriterias) {
+		this.mShaderSourceCriterias = pShaderSourceCriterias;
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public boolean isMet() {
+		for(IShaderSourceCriteria shaderSourceCriteria : this.mShaderSourceCriterias) {
+			if(!shaderSourceCriteria.isMet()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

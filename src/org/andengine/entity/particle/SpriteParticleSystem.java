a8ca1a3af162483816431865a2fc583e69b110d4
Nicolas Gramlich <ngramlich@zynga.com>
2011-12-04 21:07:07 -0500

    Added SpriteParticleSystem.

---
+++ src/org/andengine/entity/particle/SpriteParticleSystem.java
@@ -0,0 +1,51 @@
+package org.andengine.entity.particle;
+
+import org.andengine.entity.IEntityFactory;
+import org.andengine.entity.particle.emitter.IParticleEmitter;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.region.ITextureRegion;
+
+/**
+ * (c) Zynga 2011
+ *
+ * @author Nicolas Gramlich <ngramlich@zynga.com>
+ * @since 21:00:23 - 04.12.2011
+ */
+public class SpriteParticleSystem extends ParticleSystem<Sprite> {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public SpriteParticleSystem(final IParticleEmitter pParticleEmitter, float pRateMinimum, float pRateMaximum, int pParticlesMaximum, final ITextureRegion pTextureRegion) {
+		super(new IEntityFactory<Sprite>() {
+			@Override
+			public Sprite create(float pX, float pY) {
+				return new Sprite(pX, pY, pTextureRegion);
+			}
+		}, pParticleEmitter, pRateMinimum, pRateMaximum, pParticlesMaximum);
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

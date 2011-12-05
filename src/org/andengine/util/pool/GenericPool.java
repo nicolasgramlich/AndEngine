794b347baf77d09cc9d85c175af16dd97831d24d
Nicolas Gramlich <ngramlich@zynga.com>
2011-12-05 17:54:40 -0500

    Removed ADT16-Preview BuildConfig.DEBUG condition around log statement in GenericPool.

--- src/org/andengine/util/pool/GenericPool.java
+++ src/org/andengine/util/pool/GenericPool.java
@@ -132,9 +132,9 @@ public abstract class GenericPool<T> {
 				this.batchAllocatePoolItems(this.mGrowth);
 				item = this.mAvailableItems.remove(this.mAvailableItems.size() - 1);
 			}
-			if(BuildConfig.DEBUG) {
+//			if(BuildConfig.DEBUG) {
 				Debug.i(this.getClass().getName() + "<" + item.getClass().getSimpleName() +"> was exhausted, with " + this.mUnrecycledItemCount + " item not yet recycled. Allocated " + this.mGrowth + " more.");
-			}
+//			}
 		}
 		this.onHandleObtainItem(item);
 

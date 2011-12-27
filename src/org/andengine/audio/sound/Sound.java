ef7a2ce1d6efe2a4f75980e561020801be23fd5b
Nicolas Gramlich <ngramlich@zynga.com>
2011-12-27 14:47:03 +0100

    Fixed a bug in changing the volume of a Sound. (Thx to blissoft.)

--- src/org/andengine/audio/sound/Sound.java
+++ src/org/andengine/audio/sound/Sound.java
@@ -149,8 +149,6 @@ public class Sound extends BaseAudioEntity {
 
 	@Override
 	public void onMasterVolumeChanged(final float pMasterVolume) throws SoundReleasedException {
-		this.onMasterVolumeChanged(pMasterVolume);
-
 		this.setVolume(this.mLeftVolume, this.mRightVolume);
 	}
 

3d8fea8d702e455d5fdecb851b842caa354eff7b
Nicolas Gramlich <ngramlich@zynga.com>
2011-12-28 17:27:11 +0100

    Codestyle adaptions in BlendFunctionInitializer.

--- src/org/andengine/entity/particle/initializer/BlendFunctionInitializer.java
+++ src/org/andengine/entity/particle/initializer/BlendFunctionInitializer.java
@@ -3,30 +3,57 @@ package org.andengine.entity.particle.initializer;
 import org.andengine.entity.particle.Particle;
 import org.andengine.entity.shape.Shape;
 
+import android.opengl.GLES20;
+
 /**
- * Sets the blend function used to draw the particle during it's lifespan. Only applicable to Shapes.
+ * Sets the blend function used to draw the {@link Particle} during its lifespan. Only applicable to {@link Shape}s.
  * 
  * @author janne.sinivirta
- *
- * @param <T> type of particle
+ * 
+ * @since 17:24:32 - 28.12.2011
  */
 public class BlendFunctionInitializer<T extends Shape> implements IParticleInitializer<T> {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
 
 	protected int mSourceBlendFunction;
 	protected int mDestinationBlendFunction;
-	
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
 	/**
-	 * Blend function set for each spawned particle. Use GLES20 constants for setting functions.
-	 * @param pSourceBlendFunction
-	 * @param pDestinationBlendFunction
+	 * Blend function set for each spawned {@link Particle}. Use {@link GLES20} constants for setting functions.
 	 */
 	public BlendFunctionInitializer(final int pSourceBlendFunction, final int pDestinationBlendFunction) {
 		this.mSourceBlendFunction = pSourceBlendFunction;
 		this.mDestinationBlendFunction = pDestinationBlendFunction;
 	}
 
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
 	@Override
-	public void onInitializeParticle(Particle<T> pParticle) {
-		pParticle.getEntity().setBlendFunction(mSourceBlendFunction, mDestinationBlendFunction);
+	public void onInitializeParticle(final Particle<T> pParticle) {
+		pParticle.getEntity().setBlendFunction(this.mSourceBlendFunction, this.mDestinationBlendFunction);
 	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
 }

2c504265c57e79970d2a25147a80b9e31eeab7ae
Janne Sinivirta <janne.sinivirta@gmail.com>
2011-12-29 09:59:47 +0200

    enable drawing of children behind the parent. default is in front

--- src/org/andengine/entity/Entity.java
+++ src/org/andengine/entity/Entity.java
@@ -54,7 +54,8 @@ public class Entity implements IEntity {
 	protected boolean mChildrenVisible = true;
 	protected boolean mChildrenIgnoreUpdate = false;
 	protected boolean mChildrenSortPending = false;
-
+	protected boolean mDrawChildrenBehindParent = false;
+	
 	protected int mZIndex = 0;
 
 	private IEntity mParent;
@@ -441,6 +442,16 @@ public class Entity implements IEntity {
 		return this.mRotation != 0 || this.mScaleX != 1 || this.mScaleY != 1 || this.mSkewX != 0 || this.mSkewY != 0;
 	}
 
+	@Override
+	public boolean isDrawChildrenBehindParent() {
+		return mDrawChildrenBehindParent;
+	}
+	
+	@Override
+	public void setDrawChildrenBehindParent(final boolean pDrawChildrenBehindParent) {
+		this.mDrawChildrenBehindParent = pDrawChildrenBehindParent;
+	}
+	
 	@Override
 	public float getRed() {
 		return this.mColor.getRed();
@@ -1178,11 +1189,13 @@ public class Entity implements IEntity {
 		{
 			this.onApplyTransformations(pGLState);
 
+			if (mDrawChildrenBehindParent) this.onDrawChildren(pGLState, pCamera);
+			
 			this.preDraw(pGLState, pCamera);
 			this.draw(pGLState, pCamera);
 			this.postDraw(pGLState, pCamera);
 
-			this.onDrawChildren(pGLState, pCamera);
+			if (!mDrawChildrenBehindParent) this.onDrawChildren(pGLState, pCamera);
 		}
 		pGLState.popModelViewGLMatrix();
 	}
--- src/org/andengine/entity/IEntity.java
+++ src/org/andengine/entity/IEntity.java
@@ -112,6 +112,13 @@ public interface IEntity extends IDrawHandler, IUpdateHandler {
 	public void setColor(final float pRed, final float pGreen, final float pBlue, final float pAlpha);
 
 	/**
+	 * Do we draw Entity's children on top or behind the Entity. Default is on top (false).
+	 * @return false by default
+	 */
+	public boolean isDrawChildrenBehindParent();
+	public void setDrawChildrenBehindParent(final boolean pDrawChildrenBehindParent);
+	
+	/**
 	 * @return a shared(!) float[] of length 2.
 	 */
 	public float[] getSceneCenterCoordinates();

0f5157e7fbc27722ea6b8a750bcba91f9366c4f6
Nicolas Gramlich <ngramlich@zynga.com>
2012-01-06 15:00:03 -0800

    Fixed missing header in BufferUtils.h

--- jni/src/BufferUtils.h
+++ jni/src/BufferUtils.h
@@ -7,4 +7,5 @@ extern "C" {
 	
 	JNIEXPORT void JNICALL Java_org_andengine_opengl_util_BufferUtils_jniPut(JNIEnv *, jclass, jobject, jfloatArray, jint, jint);
 	JNIEXPORT jobject JNICALL Java_org_andengine_opengl_util_BufferUtils_jniAllocateDirect(JNIEnv *, jclass, jint);
+	JNIEXPORT void JNICALL Java_org_andengine_opengl_util_BufferUtils_jniFreeDirect(JNIEnv *, jclass, jobject);
 }

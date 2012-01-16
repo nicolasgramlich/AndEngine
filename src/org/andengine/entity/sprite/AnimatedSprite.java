8447b5bfbe834435c5ab57157d080e48c8208d91
Nicolas Gramlich <ngramlich@zynga.com>
2012-01-16 01:29:30 -0800

    Improved AnimatedSprite.IAnimationListener with more callbacks.

--- src/org/andengine/entity/sprite/AnimatedSprite.java
+++ src/org/andengine/entity/sprite/AnimatedSprite.java
@@ -22,6 +22,7 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 	// ===========================================================
 
 	private static final int LOOP_CONTINUOUS = -1;
+	private static final int FRAMEINDEX_INVALID = -1;
 
 	// ===========================================================
 	// Fields
@@ -35,11 +36,13 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 
 	private int mFirstTileIndex;
 	private int mInitialLoopCount;
-	private int mLoopCount;
+	private int mRemainingLoopCount;
 	private IAnimationListener mAnimationListener;
 
+	private int mCurrentFrameIndex;
 	private int mFrameCount;
 	private int[] mFrames;
+	private boolean mAnimationStartedFired;
 
 	// ===========================================================
 	// Constructors
@@ -109,29 +112,59 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 	protected void onManagedUpdate(final float pSecondsElapsed) {
 		super.onManagedUpdate(pSecondsElapsed);
 		if(this.mAnimationRunning) {
+			if(!this.mAnimationStartedFired && this.mAnimationProgress == 0 ) {
+				this.mAnimationStartedFired = true;
+				if(this.mFrames == null) {
+					this.setCurrentTileIndex(this.mFirstTileIndex);
+				} else {
+					this.setCurrentTileIndex(this.mFrames[0]);
+				}
+				this.mCurrentFrameIndex = 0;
+				if(this.mAnimationListener != null) {
+					this.mAnimationListener.onAnimationStarted(this, this.mInitialLoopCount);
+					this.mAnimationListener.onAnimationFrameChanged(this, AnimatedSprite.FRAMEINDEX_INVALID, 0);
+				}
+			}
 			final long nanoSecondsElapsed = (long) (pSecondsElapsed * TimeConstants.NANOSECONDS_PER_SECOND);
 			this.mAnimationProgress += nanoSecondsElapsed;
 
-			if(this.mAnimationProgress > this.mAnimationDuration) {
-				this.mAnimationProgress %= this.mAnimationDuration;
-				if(this.mInitialLoopCount != AnimatedSprite.LOOP_CONTINUOUS) {
-					this.mLoopCount--;
+			if(this.mInitialLoopCount == AnimatedSprite.LOOP_CONTINUOUS) {
+				while(this.mAnimationProgress > this.mAnimationDuration) {
+					this.mAnimationProgress -= this.mAnimationDuration;
+					if(this.mAnimationListener != null) {
+						this.mAnimationListener.onAnimationLoopFinished(this, this.mRemainingLoopCount, this.mInitialLoopCount);
+					}
+				}
+			} else {
+				while(this.mAnimationProgress > this.mAnimationDuration) {
+					this.mAnimationProgress -= this.mAnimationDuration;
+					this.mRemainingLoopCount--;
+					if(this.mRemainingLoopCount < 0) {
+						break;
+					} else if(this.mAnimationListener != null) {
+						this.mAnimationListener.onAnimationLoopFinished(this, this.mRemainingLoopCount, this.mInitialLoopCount);
+					}
 				}
 			}
 
-			if(this.mInitialLoopCount == AnimatedSprite.LOOP_CONTINUOUS || this.mLoopCount >= 0) {
-				final int currentFrameIndex = this.calculateCurrentFrameIndex();
+			if(this.mInitialLoopCount == AnimatedSprite.LOOP_CONTINUOUS || this.mRemainingLoopCount >= 0) {
+				final int newFrameIndex = this.calculateCurrentFrameIndex();
 
-				if(this.mFrames == null) {
-					this.setCurrentTileIndex(this.mFirstTileIndex + currentFrameIndex);
-				} else {
-					this.setCurrentTileIndex(this.mFrames[currentFrameIndex]);
+				if(this.mCurrentFrameIndex != newFrameIndex) {
+					if(this.mFrames == null) {
+						this.setCurrentTileIndex(this.mFirstTileIndex + newFrameIndex);
+					} else {
+						this.setCurrentTileIndex(this.mFrames[newFrameIndex]);
+					}
+					if(this.mAnimationListener != null) {
+						this.mAnimationListener.onAnimationFrameChanged(this, this.mCurrentFrameIndex, newFrameIndex);
+					}
 				}
-				this.mAnimationListener.onPerformedFrameAnimation(currentFrameIndex);
+				this.mCurrentFrameIndex = newFrameIndex;
 			} else {
 				this.mAnimationRunning = false;
 				if(this.mAnimationListener != null) {
-					this.mAnimationListener.onAnimationEnd(this);
+					this.mAnimationListener.onAnimationFinished(this);
 				}
 			}
 		}
@@ -167,16 +200,20 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 		return this.animate(pFrameDurationEach, true);
 	}
 
+	public AnimatedSprite animate(final long pFrameDurationEach, final IAnimationListener pAnimationListener) {
+		return this.animate(pFrameDurationEach, true, pAnimationListener);
+	}
+
 	public AnimatedSprite animate(final long pFrameDurationEach, final boolean pLoop) {
 		return this.animate(pFrameDurationEach, (pLoop) ? AnimatedSprite.LOOP_CONTINUOUS : 0, null);
 	}
 
-	public AnimatedSprite animate(final long pFrameDurationEach, final int pLoopCount) {
-		return this.animate(pFrameDurationEach, pLoopCount, null);
-	}
-
 	public AnimatedSprite animate(final long pFrameDurationEach, final boolean pLoop, final IAnimationListener pAnimationListener) {
 		return this.animate(pFrameDurationEach, (pLoop) ? AnimatedSprite.LOOP_CONTINUOUS : 0, pAnimationListener);
+	}
+
+	public AnimatedSprite animate(final long pFrameDurationEach, final int pLoopCount) {
+		return this.animate(pFrameDurationEach, pLoopCount, null);
 	}
 
 	public AnimatedSprite animate(final long pFrameDurationEach, final int pLoopCount, final IAnimationListener pAnimationListener) {
@@ -189,16 +226,20 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 		return this.animate(pFrameDurations, true);
 	}
 
+	public AnimatedSprite animate(final long[] pFrameDurations, final IAnimationListener pAnimationListener) {
+		return this.animate(pFrameDurations, true, pAnimationListener);
+	}
+
 	public AnimatedSprite animate(final long[] pFrameDurations, final boolean pLoop) {
 		return this.animate(pFrameDurations, (pLoop) ? AnimatedSprite.LOOP_CONTINUOUS : 0, null);
 	}
 
-	public AnimatedSprite animate(final long[] pFrameDurations, final int pLoopCount) {
-		return this.animate(pFrameDurations, pLoopCount, null);
-	}
-
 	public AnimatedSprite animate(final long[] pFrameDurations, final boolean pLoop, final IAnimationListener pAnimationListener) {
 		return this.animate(pFrameDurations, (pLoop) ? AnimatedSprite.LOOP_CONTINUOUS : 0, pAnimationListener);
+	}
+
+	public AnimatedSprite animate(final long[] pFrameDurations, final int pLoopCount) {
+		return this.animate(pFrameDurations, pLoopCount, null);
 	}
 
 	public AnimatedSprite animate(final long[] pFrameDurations, final int pLoopCount, final IAnimationListener pAnimationListener) {
@@ -207,6 +248,10 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 
 	public AnimatedSprite animate(final long[] pFrameDurations, final int pFirstTileIndex, final int pLastTileIndex, final boolean pLoop) {
 		return this.animate(pFrameDurations, pFirstTileIndex, pLastTileIndex, (pLoop) ? AnimatedSprite.LOOP_CONTINUOUS : 0, null);
+	}
+
+	public AnimatedSprite animate(final long[] pFrameDurations, final int pFirstTileIndex, final int pLastTileIndex, final boolean pLoop, final IAnimationListener pAnimationListener) {
+		return this.animate(pFrameDurations, pFirstTileIndex, pLastTileIndex, (pLoop) ? AnimatedSprite.LOOP_CONTINUOUS : 0, pAnimationListener);
 	}
 
 	public AnimatedSprite animate(final long[] pFrameDurations, final int pFirstTileIndex, final int pLastTileIndex, final int pLoopCount) {
@@ -218,7 +263,7 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 	}
 
 	/**
-	 * Animate specifics frames
+	 * Animate specifics frames.
 	 * 
 	 * @param pFrameDurations must have the same length as pFrames.
 	 * @param pFrames indices of the frames to animate.
@@ -235,9 +280,7 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 	}
 
 	/**
-	 * @param pFrameDurations
-	 *            must have the same length as pFirstTileIndex to
-	 *            pLastTileIndex.
+	 * @param pFrameDurations must have the same length as pFirstTileIndex to pLastTileIndex.
 	 * @param pFirstTileIndex
 	 * @param pLastTileIndex
 	 * @param pLoopCount
@@ -256,11 +299,12 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 		return this.init(pFrameDurations, frameCount, null, pFirstTileIndex, pLoopCount, pAnimationListener);
 	}
 
-	private AnimatedSprite init(final long[] pFrameDurations, final int frameCount, final int[] pFrames, final int pFirstTileIndex, final int pLoopCount, final IAnimationListener pAnimationListener) {
-		this.mFrameCount = frameCount;
+	private AnimatedSprite init(final long[] pFrameDurations, final int pFrameCount, final int[] pFrames, final int pFirstTileIndex, final int pLoopCount, final IAnimationListener pAnimationListener) {
+		this.mAnimationStartedFired = false;
+		this.mFrameCount = pFrameCount;
 		this.mAnimationListener = pAnimationListener;
 		this.mInitialLoopCount = pLoopCount;
-		this.mLoopCount = pLoopCount;
+		this.mRemainingLoopCount = pLoopCount;
 		this.mFrames = pFrames;
 		this.mFirstTileIndex = pFirstTileIndex;
 
@@ -293,7 +337,19 @@ public class AnimatedSprite extends TiledSprite implements TimeConstants {
 		// Fields
 		// ===========================================================
 
-		public void onPerformedFrameAnimation(final int pPerformedFrame);
-		public void onAnimationEnd(final AnimatedSprite pAnimatedSprite);
+		public void onAnimationStarted(final AnimatedSprite pAnimatedSprite, final int pInitialLoopCount);
+		/**
+		 * @param pAnimatedSprite
+		 * @param pOldFrameIndex equals {@link AnimatedSprite#FRAMEINDEX_INVALID}, the first time {@link IAnimationListener#onAnimationFrameChanged(AnimatedSprite, int, int)} is called.
+		 * @param pNewFrameIndex the new frame index of the currently active animation.
+		 */
+		public void onAnimationFrameChanged(final AnimatedSprite pAnimatedSprite, final int pOldFrameIndex, final int pNewFrameIndex);
+		/**
+		 * @param pAnimatedSprite
+		 * @param pRemainingLoopCount is {@link AnimatedSprite#LOOP_CONTINUOUS} when {@link AnimatedSprite} loops infinitely.
+		 * @param pInitialLoopCount is {@link AnimatedSprite#LOOP_CONTINUOUS} when {@link AnimatedSprite} loops infinitely.
+		 */
+		public void onAnimationLoopFinished(final AnimatedSprite pAnimatedSprite, final int pRemainingLoopCount, final int pInitialLoopCount);
+		public void onAnimationFinished(final AnimatedSprite pAnimatedSprite);
 	}
 }

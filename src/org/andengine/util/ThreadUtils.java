5bbbb5426c2848f2364b4dd1c7c370e92c2eab14
Nicolas Gramlich <ngramlich@zynga.com>
2012-01-19 12:39:54 -0800

    Added ThreadUtils.

---
+++ src/org/andengine/util/ThreadUtils.java
@@ -0,0 +1,54 @@
+package org.andengine.util;
+
+import org.andengine.util.debug.Debug;
+import org.andengine.util.debug.Debug.DebugLevel;
+
+/**
+ * (c) Zynga 2012
+ *
+ * @author Nicolas Gramlich <ngramlich@zynga.com>
+ * @since 12:23:06 - 19.01.2012
+ */
+public class ThreadUtils {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int STACKTRACE_CALLER_DEPTH = 3;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	public static void dumpCurrentThreadInfo() {
+		ThreadUtils.dumpCurrentThreadInfo(DebugLevel.DEBUG, Thread.currentThread().getStackTrace()[ThreadUtils.STACKTRACE_CALLER_DEPTH]);
+	}
+
+	public static void dumpCurrentThreadInfo(final DebugLevel pDebugLevel) {
+		ThreadUtils.dumpCurrentThreadInfo(pDebugLevel, Thread.currentThread().getStackTrace()[ThreadUtils.STACKTRACE_CALLER_DEPTH]);
+	}
+
+	private static void dumpCurrentThreadInfo(final DebugLevel pDebugLevel, final StackTraceElement pCaller) {
+		Debug.log(pDebugLevel, pCaller.getClassName() + "." + pCaller.getMethodName() + "(" + pCaller.getFileName() + ".java:" + pCaller.getLineNumber() + ") @(Thread: '" + Thread.currentThread().getName() + "')");
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

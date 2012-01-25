c7925f31de1840278e76211b7ee77a5cb2977092
Nicolas Gramlich <ngramlich@zynga.com>
2012-01-25 13:01:30 -0800

    Fixed typo causing PinchZoomDetector to calculate the pinch distance wrong. (Thanks to Eric Fung)

--- src/org/andengine/input/touch/detector/PinchZoomDetector.java
+++ src/org/andengine/input/touch/detector/PinchZoomDetector.java
@@ -112,7 +112,7 @@ public class PinchZoomDetector extends BaseDetector {
 	 * Calculate the euclidian distance between the first two fingers.
 	 */
 	private static float calculatePointerDistance(final MotionEvent pMotionEvent) {
-		return MathUtils.distance(pMotionEvent.getX(0), pMotionEvent.getY(0), pMotionEvent.getX(1), pMotionEvent.getX(1));
+		return MathUtils.distance(pMotionEvent.getX(0), pMotionEvent.getY(0), pMotionEvent.getX(1), pMotionEvent.getY(1));
 	}
 
 	// ===========================================================

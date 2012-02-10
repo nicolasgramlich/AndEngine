90fbbf6b0e8f85d217fd1f823fb2a882fb6a5ea3
Nicolas Gramlich <ngramlich@zynga.com>
2012-02-09 21:15:13 -0800

    Fixed JarvisMarch against edge cases.

--- src/org/andengine/util/algorithm/hull/JarvisMarch.java
+++ src/org/andengine/util/algorithm/hull/JarvisMarch.java
@@ -1,5 +1,6 @@
 package org.andengine.util.algorithm.hull;
 
+import org.andengine.util.math.MathConstants;
 import org.andengine.util.math.MathUtils;
 
 
@@ -43,8 +44,12 @@ public class JarvisMarch implements IHullAlgorithm {
 	// ===========================================================
 
 	private int jarvisMarch(final float[] pVertices, final int pVertexCount, final int pVertexOffsetX, final int pVertexOffsetY, final int pStride) {
-		int currentHullVertexIndex = HullUtils.indexOfLowestVertex(pVertices, pVertexCount, pVertexOffsetY, pStride);
+		final int firstHullVertexIndex = HullUtils.indexOfLowestVertex(pVertices, pVertexCount, pVertexOffsetY, pStride);
+		final float firstHullVertexX = HullUtils.getVertex(pVertices, pVertexOffsetX, pStride, firstHullVertexIndex);
+		final float firstHullVertexY = HullUtils.getVertex(pVertices, pVertexOffsetY, pStride, firstHullVertexIndex);
+
 		int hullVertexCount = 0;
+		int currentHullVertexIndex = firstHullVertexIndex;
 		float currentHullVertexAngle = 0;
 		do {
 			HullUtils.swap(pVertices, pStride, hullVertexCount, currentHullVertexIndex);
@@ -53,33 +58,48 @@ public class JarvisMarch implements IHullAlgorithm {
 				final float currentHullPointVertexX = HullUtils.getVertex(pVertices, pVertexOffsetX, pStride, hullVertexCount);
 				final float currentHullPointVertexY = HullUtils.getVertex(pVertices, pVertexOffsetY, pStride, hullVertexCount);
 
-				/* Compute the angle between the current hull vertex and all remaining vertices. 
+				hullVertexCount++;
+
+				/* Compute the angle between the current hull vertex and all remaining vertices.
 				 * Pick the smallest angle larger then the current angle. */
 				int nextHullVertexIndex = 0;
-				float nextHullVertexAngle = MathUtils.PI_TWICE; /* 360 degrees. */
-				
+				float nextHullVertexAngle = MathConstants.PI_TWICE; /* 360 degrees. */
+
 				/* Start searching one behind the already found hull vertices. */
-				for(int j = hullVertexCount + 1; j < pVertexCount; j++) {
+				for(int j = hullVertexCount; j < pVertexCount; j++) {
 					final float possibleNextHullVertexX = HullUtils.getVertex(pVertices, pVertexOffsetX, pStride, j);
 					final float possibleNextHullVertexY = HullUtils.getVertex(pVertices, pVertexOffsetY, pStride, j);
 
 					final float dX = possibleNextHullVertexX - currentHullPointVertexX;
 					final float dY = possibleNextHullVertexY - currentHullPointVertexY;
-					
+
 					float possibleNextHullVertexAngle = MathUtils.atan2(dY, dX);
 					if(possibleNextHullVertexAngle < 0) {
-						possibleNextHullVertexAngle += MathUtils.PI_TWICE; /* 360 degrees. */
+						possibleNextHullVertexAngle += MathConstants.PI_TWICE; /* 360 degrees. */
 					}
-					if(possibleNextHullVertexAngle > currentHullVertexAngle && possibleNextHullVertexAngle < nextHullVertexAngle) {
+					if((possibleNextHullVertexAngle >= currentHullVertexAngle) && (possibleNextHullVertexAngle <= nextHullVertexAngle)) {
 						nextHullVertexIndex = j;
 						nextHullVertexAngle = possibleNextHullVertexAngle;
+					}
+				}
+
+				/* Compare against first hull vertex. */
+				if(hullVertexCount > 1) {
+					final float dX = firstHullVertexX - currentHullPointVertexX;
+					final float dY = firstHullVertexY - currentHullPointVertexY;
+
+					float possibleNextHullVertexAngle = MathUtils.radToDeg(MathUtils.atan2(dY, dX));
+					if(possibleNextHullVertexAngle < 0) {
+						possibleNextHullVertexAngle += MathConstants.PI_TWICE; /* 360 degrees. */
+					}
+					if((possibleNextHullVertexAngle >= currentHullVertexAngle) && (possibleNextHullVertexAngle <= nextHullVertexAngle)) {
+						break;
 					}
 				}
 
 				currentHullVertexAngle = nextHullVertexAngle;
 				currentHullVertexIndex = nextHullVertexIndex;
 			}
-			hullVertexCount++;
 		} while(currentHullVertexIndex > 0);
 
 		return hullVertexCount;

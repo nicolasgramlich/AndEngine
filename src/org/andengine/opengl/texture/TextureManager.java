a12f29dbee06c5399c94fd67c1ac18ed76545365
Nicolas Gramlich <ngramlich@zynga.com>
2012-02-23 12:05:53 -0800

    Added methods to synchronously load Textures, instead of on the next frame.

--- src/org/andengine/opengl/texture/TextureManager.java
+++ src/org/andengine/opengl/texture/TextureManager.java
@@ -104,11 +104,9 @@ public class TextureManager {
 	public synchronized void addMappedTexture(final String pID, final ITexture pTexture) throws IllegalArgumentException {
 		if(pID == null) {
 			throw new IllegalArgumentException("pID must not be null!");
-		}
-		if(pTexture == null) {
+		} else if(pTexture == null) {
 			throw new IllegalArgumentException("pTexture must not be null!");
-		}
-		if(this.mTexturesMapped.containsKey(pID)) {
+		} else if(this.mTexturesMapped.containsKey(pID)) {
 			throw new IllegalArgumentException("Collision for pID: '" + pID + "'.");
 		}
 		this.mTexturesMapped.put(pID, pTexture);
@@ -129,6 +127,7 @@ public class TextureManager {
 		if(pTexture == null) {
 			throw new IllegalArgumentException("pTexture must not be null!");
 		}
+
 		if(this.mTexturesManaged.contains(pTexture)) {
 			/* Just make sure it doesn't get deleted. */
 			this.mTexturesToBeUnloaded.remove(pTexture);
@@ -141,6 +140,35 @@ public class TextureManager {
 	}
 
 	/**
+	 * Must be called from the GL-{@link Thread}.
+	 *
+	 * @param pGLState
+	 * @param pTexture the {@link ITexture} to be loaded right now, if it is not loaded.
+	 * @return <code>true</code> when the {@link ITexture} was previously not managed by this {@link TextureManager}, <code>false</code> if it was already managed.
+	 */
+	public synchronized boolean loadTexture(final GLState pGLState, final ITexture pTexture) throws IOException {
+		if(pTexture == null) {
+			throw new IllegalArgumentException("pTexture must not be null!");
+		}
+
+		if(!pTexture.isLoadedToHardware()) {
+			pTexture.loadToHardware(pGLState);
+		} else if(pTexture.isUpdateOnHardwareNeeded()) {
+			pTexture.reloadToHardware(pGLState);
+		}
+
+		if(this.mTexturesManaged.contains(pTexture)) {
+			/* Just make sure it doesn't get deleted. */
+			this.mTexturesToBeUnloaded.remove(pTexture);
+			return false;
+		} else {
+			this.mTexturesManaged.add(pTexture);
+			this.mTexturesLoaded.add(pTexture);
+			return true;
+		}
+	}
+
+	/**
 	 * @param pTexture the {@link ITexture} to be unloaded before the very next frame is drawn (Or prevent it from being loaded then).
 	 * @return <code>true</code> when the {@link ITexture} was already managed by this {@link TextureManager}, <code>false</code> if it was not managed.
 	 */
@@ -148,6 +176,7 @@ public class TextureManager {
 		if(pTexture == null) {
 			throw new IllegalArgumentException("pTexture must not be null!");
 		}
+
 		if(this.mTexturesManaged.contains(pTexture)) {
 			/* If the Texture is loaded, unload it.
 			 * If the Texture is about to be loaded, stop it from being loaded. */
@@ -156,6 +185,31 @@ public class TextureManager {
 			} else if(this.mTexturesToBeLoaded.remove(pTexture)) {
 				this.mTexturesManaged.remove(pTexture);
 			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Must be called from the GL-{@link Thread}.
+	 *
+	 * @param pGLState
+	 * @param pTexture the {@link ITexture} to be unloaded right now, if it is loaded.
+	 * @return <code>true</code> when the {@link ITexture} was already managed by this {@link TextureManager}, <code>false</code> if it was not managed.
+	 */
+	public synchronized boolean unloadTexture(final GLState pGLState, final ITexture pTexture) {
+		if(pTexture == null) {
+			throw new IllegalArgumentException("pTexture must not be null!");
+		} else if(pTexture.isLoadedToHardware()) {
+			pTexture.unloadFromHardware(pGLState);
+		}
+
+		if(this.mTexturesManaged.contains(pTexture)) {
+			/* Just make sure it doesn't get loaded. */
+			this.mTexturesLoaded.remove(pTexture);
+			this.mTexturesToBeLoaded.remove(pTexture);
+
 			return true;
 		} else {
 			return false;
@@ -212,7 +266,7 @@ public class TextureManager {
 		}
 
 		/* Finally invoke the GC if anything has changed. */
-		if(texturesToBeLoadedCount > 0 || texturesToBeUnloadedCount > 0) {
+		if((texturesToBeLoadedCount > 0) || (texturesToBeUnloadedCount > 0)) {
 			System.gc();
 		}
 	}

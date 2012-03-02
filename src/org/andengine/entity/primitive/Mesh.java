96ae1a7c3bf74af4c3701ed8e9ad2c9fa9f71247
Nicolas Gramlich <ngramlich@zynga.com>
2012-03-02 11:08:07 -0800

    Minor changes.

--- src/org/andengine/entity/primitive/Mesh.java
+++ src/org/andengine/entity/primitive/Mesh.java
@@ -36,9 +36,9 @@ public class Mesh extends Shape {
 	public static final int VERTEX_SIZE = 2 + 1;
 
 	public static final VertexBufferObjectAttributes VERTEXBUFFEROBJECTATTRIBUTES_DEFAULT = new VertexBufferObjectAttributesBuilder(2)
-	.add(ShaderProgramConstants.ATTRIBUTE_POSITION_LOCATION, ShaderProgramConstants.ATTRIBUTE_POSITION, 2, GLES20.GL_FLOAT, false)
-	.add(ShaderProgramConstants.ATTRIBUTE_COLOR_LOCATION, ShaderProgramConstants.ATTRIBUTE_COLOR, 4, GLES20.GL_UNSIGNED_BYTE, true)
-	.build();
+		.add(ShaderProgramConstants.ATTRIBUTE_POSITION_LOCATION, ShaderProgramConstants.ATTRIBUTE_POSITION, 2, GLES20.GL_FLOAT, false)
+		.add(ShaderProgramConstants.ATTRIBUTE_COLOR_LOCATION, ShaderProgramConstants.ATTRIBUTE_COLOR, 4, GLES20.GL_UNSIGNED_BYTE, true)
+		.build();
 
 	// ===========================================================
 	// Fields

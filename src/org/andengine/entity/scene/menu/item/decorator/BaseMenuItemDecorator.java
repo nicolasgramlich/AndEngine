64d944ba202441d83ca6993bcd1894435e586b49
Nicolas Gramlich <ngramlich@zynga.com>
2012-05-01 15:54:39 -0700

    Added queryFirst and queryFirstForSubclass convenience methods to IEntity/Entity.

--- src/org/andengine/entity/scene/menu/item/decorator/BaseMenuItemDecorator.java
+++ src/org/andengine/entity/scene/menu/item/decorator/BaseMenuItemDecorator.java
@@ -640,8 +640,18 @@ public abstract class BaseMenuItemDecorator implements IMenuItem {
 	}
 
 	@Override
+	public IEntity queryFirst(final IEntityMatcher pEntityMatcher) {
+		return this.mMenuItem.queryFirst(pEntityMatcher);
+	}
+
+	@Override
 	public <L extends List<IEntity>> L query(final IEntityMatcher pEntityMatcher, final L pResult) {
 		return this.mMenuItem.query(pEntityMatcher, pResult);
+	}
+
+	@Override
+	public <S extends IEntity> S queryFirstForSubclass(final IEntityMatcher pEntityMatcher) {
+		return this.mMenuItem.queryFirstForSubclass(pEntityMatcher);
 	}
 
 	@Override

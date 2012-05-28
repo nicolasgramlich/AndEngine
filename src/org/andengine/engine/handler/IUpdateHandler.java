2b8211f448151b134feab97df7dd035469b2eda4
Maarten <maartenhoedjes@gmail.com>
2012-05-28 10:48:44 +0200

    Javadoc on IUpdateHandler

--- src/org/andengine/engine/handler/IUpdateHandler.java
+++ src/org/andengine/engine/handler/IUpdateHandler.java
@@ -1,9 +1,22 @@
 package org.andengine.engine.handler;
 
+import org.andengine.engine.Engine;
+import org.andengine.entity.scene.IOnAreaTouchListener;
+import org.andengine.entity.scene.Scene;
 import org.andengine.util.IMatcher;
 
 /**
- * (c) 2010 Nicolas Gramlich 
+ * An update handler is something which you can create, and register within your
+ * {@link Engine} or {@link Scene} using
+ * {@link Engine#registerUpdateHandler(IUpdateHandler)} or
+ * {@link Scene#registerUpdateHandler(IUpdateHandler)} so that the code in
+ * {@link #onUpdate(float)} is run on every update of the engine. If your
+ * program does not require updating on every tick, consider using a listener,
+ * like {@link IOnAreaTouchListener} for listening to when an area is touched.
+ * Also don't forget to unregister when you don't need the update handler
+ * anymore.<br>
+ * <br>
+ * (c) 2010 Nicolas Gramlich <br>
  * (c) 2011 Zynga Inc.
  * 
  * @author Nicolas Gramlich
@@ -18,15 +31,26 @@ public interface IUpdateHandler {
 	// Methods
 	// ===========================================================
 
+	/**
+	 * The code that should happen with each tick of the engine.
+	 * 
+	 * @param pSecondsElapsed
+	 *            How many seconds have elapsed since the last update
+	 */
 	public void onUpdate(final float pSecondsElapsed);
+
+	/**
+	 * Code to perform when the handler is reset. (Gets called, for example, when the scene it's registered to is reset.)
+	 */
 	public void reset();
-	
-	// TODO Maybe add onRegister and onUnregister. (Maybe add SimpleUpdateHandler that implements all methods, but onUpdate)
+
+	// TODO Maybe add onRegister and onUnregister. (Maybe add
+	// SimpleUpdateHandler that implements all methods, but onUpdate)
 
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
-	
+
 	public interface IUpdateHandlerMatcher extends IMatcher<IUpdateHandler> {
 		// ===========================================================
 		// Constants

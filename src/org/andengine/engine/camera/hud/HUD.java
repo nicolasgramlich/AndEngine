a9b7da43c6534f4f5e212e34fad9df9810eb5cd7
Maarten <maartenhoedjes@gmail.com>
2012-05-28 11:03:41 +0200

    Javadoc on HUD

--- src/org/andengine/engine/camera/hud/HUD.java
+++ src/org/andengine/engine/camera/hud/HUD.java
@@ -5,11 +5,17 @@ import org.andengine.entity.scene.CameraScene;
 import org.andengine.entity.scene.Scene;
 
 /**
- * While you can add a {@link HUD} to {@link Scene}, you should not do so.
- * {@link HUD}s are meant to be added to {@link Camera}s via {@link Camera#setHUD(HUD)}.
- * 
- * (c) 2010 Nicolas Gramlich 
+ * While you can add a {@link HUD} to a {@link Scene}, you should not do so.
+ * {@link HUD}s are meant to be added to {@link Camera}s via
+ * {@link Camera#setHUD(HUD)}. <br>
+ * HUD is the acronym of head-up display and it represents all those parts of
+ * the user interface that are placed on a fixed position on the display. In a
+ * game this normally includes life bars, ammunition count, maps, level, etc. <br>
+ * (c) 2010 Nicolas Gramlich <br>
  * (c) 2011 Zynga Inc.
+ * 
+ * 
+ * (c) 2010 Nicolas Gramlich (c) 2011 Zynga Inc.
  * 
  * @author Nicolas Gramlich
  * @since 14:13:13 - 01.04.2010

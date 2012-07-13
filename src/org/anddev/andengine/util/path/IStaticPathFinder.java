38fa99359a10b0029092f5529f7e60b05014f47b
Winnie <git@winniehell.de>
2012-07-13 17:19:20 +0300

    remove wrong copyright in IStaticPathFinder

--- src/org/anddev/andengine/util/path/IStaticPathFinder.java
+++ src/org/anddev/andengine/util/path/IStaticPathFinder.java
@@ -2,13 +2,6 @@ package org.anddev.andengine.util.path;
 
 import org.anddev.andengine.util.path.floyd_warshall.NegativeCycleException;
 
-/**
- * (c) 2010 Nicolas Gramlich 
- * (c) 2011 Zynga Inc.
- * 
- * @author Nicolas Gramlich
- * @since 22:57:13 - 16.08.2010
- */
 public interface IStaticPathFinder<T> {
 	// ===========================================================
 	// Constants

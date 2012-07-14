b7de442a74bd04f1d6a16888f55f843877373801
Michal Stawinski <michal.stawinski@gmail.com>
2012-07-15 00:06:15 +0200

    Add LevelLoaderUtils class
    This class simplifies working with XML levels. It allows easy
    setting some basic parameters of an IEntity, as well as introduces
    some methods to dump current level to a file using XMLSerializer.

--- src/org/andengine/util/level/constants/LevelConstants.java
+++ src/org/andengine/util/level/constants/LevelConstants.java
@@ -19,6 +19,22 @@ public interface LevelConstants {
 	public static final String TAG_LEVEL_ATTRIBUTE_WIDTH = "width";
 	public static final String TAG_LEVEL_ATTRIBUTE_HEIGHT = "height";
 
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_POSITION_X = "x";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_POSITION_Y = "y";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_WIDTH = "width";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_HEIGHT = "height";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_SCALE_X = "scale_x";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_SCALE_Y = "scale_y";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_ROTATION = "rotation";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_ZINDEX = "zindex";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_COLOR_R = "color_r";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_COLOR_G = "color_g";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_COLOR_B = "color_b";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_FLIP_HOR = "flip_hor";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_FLIP_VER= "flip_ver";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_BLEND_SOURCE = "blend_src";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_BLEND_DESTINATION = "blend_dst";
+
 	// ===========================================================
 	// Methods
 	// ===========================================================

ff7f4787750d0ef8a773b913cb32e0b05aee954f
Nicolas Gramlich <ngramlich@zynga.com>
2013-03-17 16:34:18 -0700

    Added MethodNotFoundException.

---
+++ src/org/andengine/util/exception/MethodNotFoundException.java
@@ -0,0 +1,55 @@
+package org.andengine.util.exception;
+
+/**
+ * (c) Nicolas Gramlich 2013
+ *
+ * @author Nicolas Gramlich <ngramlich@zynga.com>
+ * @since 18:13:32 - 26.12.2011
+ */
+public class MethodNotFoundException extends AndEngineRuntimeException {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final long serialVersionUID = -6878670836555401945L;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public MethodNotFoundException() {
+
+	}
+
+	public MethodNotFoundException(final String pMessage) {
+		super(pMessage);
+	}
+
+	public MethodNotFoundException(final Throwable pThrowable) {
+		super(pThrowable);
+	}
+
+	public MethodNotFoundException(final String pMessage, final Throwable pThrowable) {
+		super(pMessage, pThrowable);
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

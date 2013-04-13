1ce99e68d49fac9ed8a978bb49314e7c8358dece
Nicolas Gramlich <ngramlich@zynga.com>
2013-04-12 19:45:39 -0700

    Added support for null Experiment values, when the 'value' attribute isn't present.

--- src/org/andengine/util/experiment/ExperimentFactory.java
+++ src/org/andengine/util/experiment/ExperimentFactory.java
@@ -37,40 +37,80 @@ public class ExperimentFactory implements IExperimentFactory {
 		final String type = pExperimentJSONObject.getString("type");
 
 		if (type.equals(Boolean.class.getSimpleName())) {
-			final boolean value = pExperimentJSONObject.getBoolean("value");
+			final Boolean value;
+			if (pExperimentJSONObject.has("value")) {
+				value = pExperimentJSONObject.getBoolean("value");
+			} else {
+				value = null;
+			}
 			return new Experiment<Boolean>(name, Boolean.class, value);
 		} else if (type.equals(Byte.class.getSimpleName())) {
-			final byte value = (byte) pExperimentJSONObject.getInt("value");
+			final Byte value;
+			if (pExperimentJSONObject.has("value")) {
+				value = (byte) pExperimentJSONObject.getInt("value");
+			} else {
+				value = null;
+			}
 			return new Experiment<Byte>(name, Byte.class, value);
 		} else if (type.equals(Character.class.getSimpleName())) {
-			final char value = (char) pExperimentJSONObject.getInt("value");
+			final Character value;
+			if (pExperimentJSONObject.has("value")) {
+				value = (char) pExperimentJSONObject.getInt("value");
+			} else {
+				value = null;
+			}
 			return new Experiment<Character>(name, Character.class, value);
 		} else if (type.equals(Short.class.getSimpleName())) {
-			final short value = (short) pExperimentJSONObject.getInt("value");
+			final Short value;
+			if (pExperimentJSONObject.has("value")) {
+				value = (short) pExperimentJSONObject.getInt("value");
+			} else {
+				value = null;
+			}
 			return new Experiment<Short>(name, Short.class, value);
 		} else if (type.equals(Integer.class.getSimpleName())) {
-			final int value = pExperimentJSONObject.getInt("value");
+			final Integer value;
+			if (pExperimentJSONObject.has("value")) {
+				value = pExperimentJSONObject.getInt("value");
+			} else {
+				value = null;
+			}
 			return new Experiment<Integer>(name, Integer.class, value);
 		} else if (type.equals(Long.class.getSimpleName())) {
-			final long value = pExperimentJSONObject.getLong("value");
+			final Long value;
+			if (pExperimentJSONObject.has("value")) {
+				value = pExperimentJSONObject.getLong("value");
+			} else {
+				value = null;
+			}
 			return new Experiment<Long>(name, Long.class, value);
 		} else if (type.equals(Float.class.getSimpleName())) {
-			final float value = (float) pExperimentJSONObject.getDouble("value");
+			final Float value;
+			if (pExperimentJSONObject.has("value")) {
+				value = (float) pExperimentJSONObject.getDouble("value");
+			} else {
+				value = null;
+			}
 			return new Experiment<Float>(name, Float.class, value);
 		} else if (type.equals(Double.class.getSimpleName())) {
-			final double value = pExperimentJSONObject.getDouble("value");
+			final Double value;
+			if (pExperimentJSONObject.has("value")) {
+				value = pExperimentJSONObject.getDouble("value");
+			} else {
+				value = null;
+			}
 			return new Experiment<Double>(name, Double.class, value);
 		} else if (type.equals(String.class.getSimpleName())) {
-			final String value = pExperimentJSONObject.getString("value");
+			final String value = pExperimentJSONObject.optString("value", null);
 			return new Experiment<String>(name, String.class, value);
 		} else if (type.equals(JSONObject.class.getSimpleName())) {
-			final JSONObject value = pExperimentJSONObject.getJSONObject("value");
+			final JSONObject value = pExperimentJSONObject.optJSONObject("value");
 			return new Experiment<JSONObject>(name, JSONObject.class, value);
 		} else if (type.equals(JSONArray.class.getSimpleName())) {
-			final JSONArray value = pExperimentJSONObject.getJSONArray("value");
+			final JSONArray value = pExperimentJSONObject.optJSONArray("value");
 			return new Experiment<JSONArray>(name, JSONArray.class, value);
 		} else {
-			final Object value = pExperimentJSONObject.get("value");
+			final Object value = pExperimentJSONObject.opt("value");
 			return new Experiment<Object>(name, Object.class, value);
 		}
 	}

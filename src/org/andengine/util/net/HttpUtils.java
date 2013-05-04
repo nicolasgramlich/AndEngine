209b472e18b1477fe8155d07c3599d8c16765669
Nicolas Gramlich <ngramlich@zynga.com>
2013-05-03 20:26:10 -0700

    Added HttpClientUtils and HttpUtils.

---
+++ src/org/andengine/util/net/HttpUtils.java
@@ -0,0 +1,66 @@
+package org.andengine.util.net;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+
+/**
+ * (c) 2013 Nicolas Gramlich
+ *
+ * @author Nicolas Gramlich
+ * @since 19:20:32 - 03.05.2013
+ */
+public final class HttpUtils {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	private HttpUtils() {
+
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	public static List<NameValuePair> convertParametersToNameValuePairs(final Map<String, String> pParameters) {
+		final List<NameValuePair> result = new ArrayList<NameValuePair>();
+
+		for (final Entry<String, String> entry : pParameters.entrySet()) {
+			result.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+		}
+
+		return result;
+	}
+
+	public static boolean isHttpStatusCodeSuccess(final int pHttpStatusCode) {
+		return pHttpStatusCode >= HttpStatus.SC_OK && pHttpStatusCode < HttpStatus.SC_MULTIPLE_CHOICES;
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+
+}

41fba3901598ca1b1adcf653f575c8e5b746cfc6
Nicolas Gramlich <ngramlich@zynga.com>
2013-05-11 13:01:59 -0700

    Added AsyncTaskUtils.

---
+++ src/org/andengine/util/AsyncTaskUtils.java
@@ -0,0 +1,55 @@
+package org.andengine.util;
+
+import android.annotation.TargetApi;
+import android.os.AsyncTask;
+import android.os.Build;
+
+
+/**
+ * (c) 2013 Nicolas Gramlich
+ *
+ * @author Nicolas Gramlich
+ * @since 12:42:12 - 11.05.2013
+ */
+public final class AsyncTaskUtils {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	private AsyncTaskUtils() {
+
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	public static <T> void executeParallel(final AsyncTask<T, ?, ?> pAsyncTask, final T ... pParameters) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			pAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, pParameters);
+		} else {
+			pAsyncTask.execute(pParameters);
+		}
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

ce7eafee48575b7ea2a201be09fec6bef53aa637
Nicolas Gramlich <ngramlich@zynga.com>
2013-06-02 01:09:15 -0700

    Added preliminary versions of LineLoop and LineStrip.

---
+++ src/org/andengine/entity/primitive/LineLoop.java
@@ -0,0 +1,70 @@
+package org.andengine.entity.primitive;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.entity.primitive.vbo.ILineChainVertexBufferObject;
+import org.andengine.opengl.util.GLState;
+import org.andengine.opengl.vbo.DrawType;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+import android.opengl.GLES20;
+
+/**
+ * (c) 2013 Nicolas Gramlich
+ *
+ * @author Nicolas Gramlich
+ * @since 13:51:36 - 01.06.2013
+ */
+public class LineLoop extends LineChain {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public LineLoop(final float pX, final float pY, final int pCapacity, final VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pCapacity, pVertexBufferObjectManager);
+	}
+
+	public LineLoop(final float pX, final float pY, final int pCapacity, final VertexBufferObjectManager pVertexBufferObjectManager, final DrawType pDrawType) {
+		super(pX, pY, pCapacity, pVertexBufferObjectManager, pDrawType);
+	}
+
+	public LineLoop(final float pX, final float pY, final float pLineWidth, final int pCapacity, final VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pLineWidth, pCapacity, pVertexBufferObjectManager);
+	}
+
+	public LineLoop(final float pX, final float pY, final float pLineWidth, final int pCapacity, final VertexBufferObjectManager pVertexBufferObjectManager, final DrawType pDrawType) {
+		super(pX, pY, pLineWidth, pCapacity, pVertexBufferObjectManager, pDrawType);
+	}
+
+	public LineLoop(final float pX, final float pY, final float pLineWidth, final int pCapacity, final ILineChainVertexBufferObject pLineChainVertexBufferObject) {
+		super(pX, pY, pLineWidth, pCapacity, pLineChainVertexBufferObject);
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	protected void draw(final GLState pGLState, final Camera pCamera) {
+		this.mLineChainVertexBufferObject.draw(GLES20.GL_LINE_LOOP, this.mIndex);
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
---
+++ src/org/andengine/entity/primitive/vbo/LowMemoryLineChainVertexBufferObject.java
@@ -0,0 +1,80 @@
+package org.andengine.entity.primitive.vbo;
+
+import java.nio.FloatBuffer;
+
+import org.andengine.entity.primitive.LineChain;
+import org.andengine.opengl.vbo.DrawType;
+import org.andengine.opengl.vbo.LowMemoryVertexBufferObject;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.opengl.vbo.attribute.VertexBufferObjectAttributes;
+
+/**
+ * (c) 2013 Nicolas Gramlich
+ *
+ * @author Nicolas Gramlich <ngramlich@zynga.com>
+ * @since 21:50:00 - 30.05.2013
+ */
+public class LowMemoryLineChainVertexBufferObject extends LowMemoryVertexBufferObject implements ILineChainVertexBufferObject {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public LowMemoryLineChainVertexBufferObject(final VertexBufferObjectManager pVertexBufferObjectManager, final int pCapacity, final DrawType pDrawType, final boolean pAutoDispose, final VertexBufferObjectAttributes pVertexBufferObjectAttributes) {
+		super(pVertexBufferObjectManager, pCapacity, pDrawType, pAutoDispose, pVertexBufferObjectAttributes);
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public void onUpdateColor(final LineChain pLineChain) {
+		final FloatBuffer bufferData = this.mFloatBuffer;
+
+		final float packedColor = pLineChain.getColor().getABGRPackedFloat();
+
+		for (int i = pLineChain.getCapacity() - 1; i >= 0; i--) {
+			bufferData.put((i * LineChain.VERTEX_SIZE) + LineChain.COLOR_INDEX, packedColor);
+		}
+
+		this.setDirtyOnHardware();
+	}
+
+	@Override
+	public void onUpdateVertices(final LineChain pLineChain) {
+		final FloatBuffer bufferData = this.mFloatBuffer;
+
+		bufferData.put((0 * LineChain.VERTEX_SIZE) + LineChain.VERTEX_INDEX_X, 0);
+		bufferData.put((0 * LineChain.VERTEX_SIZE) + LineChain.VERTEX_INDEX_Y, 0);
+
+		final float x = pLineChain.getX();
+		final float y = pLineChain.getY();
+
+		for (int i = pLineChain.getCapacity() - 1; i >= 0; i--) {
+			bufferData.put((i * LineChain.VERTEX_SIZE) + LineChain.VERTEX_INDEX_X, pLineChain.getX(i) - x);
+			bufferData.put((i * LineChain.VERTEX_SIZE) + LineChain.VERTEX_INDEX_Y, pLineChain.getY(i) - y);
+		}
+
+		this.setDirtyOnHardware();
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}

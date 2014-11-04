c73909bc7b2a9e16993783685e6c11f3711fe004
WonsonTheFantastic <wonson@gmail.com>
2014-11-04 14:53:58 +0800

    Cast to pointer first
    Fix: for Android Studio, unable to build with flag -Werror

--- jni/src/GLES20Fix.c
+++ jni/src/GLES20Fix.c
@@ -6,9 +6,9 @@
 // ===========================================================
 
 void Java_org_andengine_opengl_GLES20Fix_glVertexAttribPointer (JNIEnv *env, jclass c, jint index, jint size, jint type, jboolean normalized, jint stride, jint offset) {
-	glVertexAttribPointer(index, size, type, normalized, stride, (void*) offset);
+	glVertexAttribPointer(index, size, type, normalized, stride, (void*) (intptr_t)offset);
 }
 
 void Java_org_andengine_opengl_GLES20Fix_glDrawElements (JNIEnv *env, jclass c, jint mode, jint count, jint type, jint offset) {
-	glDrawElements(mode, count, type, (void*) offset);
+	glDrawElements(mode, count, type, (void*) (intptr_t)offset);
 }
